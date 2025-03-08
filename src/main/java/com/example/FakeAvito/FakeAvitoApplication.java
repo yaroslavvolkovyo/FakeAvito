4c3202fc75e367a7e00fe36fcfcb3a1b82571824
yaroslavvolkovyo <wartander135@gmail.com>
2025-03-08 15:15:15 +0400

    Initial commit

---
+++ src/main/java/com/example/FakeAvito/FakeAvitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.FakeAvito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeAvitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeAvitoApplication.class, args);
+	}
+
+}
